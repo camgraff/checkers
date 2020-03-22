a5317ec30e0bae1494c95738460173185737c552
camerongraff <camerongraff@mines.edu>
2020-03-22 15:08:59 -0600

    Finished implementing game logic

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 import io from 'socket.io-client';
 
-import BoardCell from "../../classes/BoardCell";
-import BoardPiece from "../../classes/BoardPiece";
+import BoardCell from '../../classes/BoardCell';
+import BoardPiece from '../../classes/BoardPiece';
 
-import "./Board.scss";
+import { Icon } from '@iconify/react';
+import chessQueen from '@iconify/icons-mdi/chess-queen';
+import './Board.scss';
 
 const ENDPOINT = 'http://localhost:5000/';
 
@@ -31,15 +33,12 @@ export default class Board extends React.Component {
                 }
             }
         }
-
         this.state = {
             board: board,
             targets: [],
             pieceToMove: null,
             player: 1
         };
-
-        
     }
 
     componentDidMount() {
@@ -52,7 +51,7 @@ export default class Board extends React.Component {
         this.setState({
             targets: this.getMoveTargets(row, col),
             pieceToMove: piece
-        })
+        });
     }
 
     // Returns an array of coordinates of valid move targets
@@ -60,37 +59,53 @@ export default class Board extends React.Component {
         const board = this.state.board;
         const piece = board[row][col].piece;
 
-        let res = []
+        let res = [];
 
-        let targets = [
-            [row-1, col-1],
-            [row+1, col-1],
-            [row-1, col+1],
-            [row+1, col+1]
-        ]
-        
-        targets.forEach(el => {
-            if (this.canMoveTo(piece, el[0], el[1])) {
-                res.push(board[el[0]][el[1]]);
+        this.getPossibleTargets(piece).forEach(adj => {
+            if (adj.hasPiece()) {
+                const jumpTarget = this.canJumpOver(piece, adj.piece);
+                if (jumpTarget !== null) {
+                    res.push(jumpTarget);
+                }
+            } else {
+                if (this.canMoveTo(piece, adj)) {
+                    res.push(adj);
+                }
             }
-        })
-
+        });
         return res;
-
     }
 
-    canMoveTo(piece, row, col) {
+    canMoveTo(piece, cell) {
         if (!piece.isKing) {
             // Player 1 cannot move down
             // Player 2 cannot move up
-            if ((piece.player === 1 && row > piece.row) || (piece.player === 2 && row < piece.row)) {
+            if ((piece.player === 1 && cell.row > piece.row) || (piece.player === 2 && cell.row < piece.row)) {
                 return false;
             }
         }
-        if (row >= 0 && col >= 0 && row < 8 && col < 8) {
-            if (!this.state.board[row][col].hasPiece()) {
-                return true;
-            }
+        if (!cell.hasPiece()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the target cell if the pieceToJump can be jumped, otherwise returns false.
+     *
+     * @param BoardPiece pieceToMove
+     * @param BoardPiece pieceToJump
+     */
+    canJumpOver(pieceToMove, pieceToJump) {
+        if (pieceToMove.player === pieceToJump.player) {
+            return false;
+        }
+        const board = this.state.board;
+        const row = 2 * (pieceToJump.row - pieceToMove.row) + pieceToMove.row;
+        const col = 2 * (pieceToJump.col - pieceToMove.col) + pieceToMove.col;
+
+        if (row >= 0 && col >= 0 && row < 8 && col < 8 && !board[row][col].hasPiece()) {
+            return board[row][col];
         }
 
         return false;
@@ -101,46 +116,104 @@ export default class Board extends React.Component {
             return;
         }
         socket.emit('move', cell, this.state.pieceToMove);
-        this.movePiece(this.state.pieceToMove, cell)
+        this.movePiece(this.state.pieceToMove, cell);
     }
 
+    /**
+     * Moves a piece on the board.
+     * Returns true if a piece was jumped, and the player can move again, false otherwise.
+     *
+     * @param BoardPiece piece
+     * @param BoardCell cell
+     *
+     */
     movePiece(piece, cell) {
         let board = this.state.board;
+        let canMoveAgain = false;
+
+        // Handle the case where we are jumping a piece
+        if (Math.abs(piece.row - cell.row) === 2) {
+            const jumpRow = (piece.row + cell.row) / 2;
+            const jumpCol = (piece.col + cell.col) / 2;
+            board[jumpRow][jumpCol].piece = null;
+            canMoveAgain = true;
+        }
 
         board[piece.row][piece.col].piece = null;
         piece.row = cell.row;
         piece.col = cell.col;
+        if (!piece.isKing && (piece.row === 0 || piece.row === 7)) {
+            piece.isKing = true;
+        }
         board[cell.row][cell.col].piece = piece;
+
+        if (canMoveAgain) {
+            //Check if another piece can be jumped
+            this.getPossibleTargets(piece).forEach(adj => {
+                if (adj.hasPiece() && this.canJumpOver(piece, adj.piece)) {
+                    console.log('move again');
+                }
+            });
+        }
 
         this.setState({
             targets: [],
             pieceToMove: null,
             board: board
         });
+
+        return canMoveAgain;
+    }
+
+    /**
+     * Returns an array of diagnolly adjacent cells.
+     *
+     * @param BoardPiece piece
+     */
+    getPossibleTargets(piece) {
+        let adj = [];
+        const directions = [
+            [1, 1],
+            [-1, 1],
+            [1, -1],
+            [-1, -1]
+        ];
+
+        directions.forEach(dir => {
+            // For non-king pieces: player 1 cannot move down, and player 2 cannot move up
+            if (!piece.isKing && ((piece.player === 1 && dir[0] === 1) || (piece.player === 2 && dir[0] === -1))) {
+                return;
+            }
+            const row = piece.row + dir[0];
+            const col = piece.col + dir[1];
+            if (row >= 0 && col >= 0 && row < 8 && col < 8) {
+                adj.push(this.state.board[row][col]);
+            }
+        });
+
+        return adj;
     }
 
     render() {
         return (
-            <div className="board">
+            <div className='board'>
                 {this.state.board.map((row, rowId) => (
-                    <div className="row" key={rowId}>
+                    <div className='row' key={rowId}>
                         {row.map((cell, colId) => (
-                            <div 
-                                className={`cell ${cell.canHavePiece() ? "moveable" : ""}`} 
+                            <div
+                                className={`cell ${cell.canHavePiece() ? 'moveable' : ''}`}
                                 key={colId}
                                 onClick={() => this.handleCellClick(cell)}
                             >
-                                {
-                                    cell.hasPiece() && 
-                                    <div 
+                                {cell.hasPiece() && (
+                                    <div
                                         className={`piece player${cell.piece.player}`}
                                         onClick={() => this.showMoveTargets(cell.piece, rowId, colId)}
-                                    />
-                                }
-                                {
-                                    this.state.targets.includes(cell) && 
-                                    <div className="target-marker"></div>
-                                }
+                                    >
+                                        {cell.piece.isKing && <Icon icon={chessQueen} className='king' />}
+                                    </div>
+                                )}
+                                {this.state.targets.includes(cell) && <div className='target-marker'></div>}
                             </div>
                         ))}
                     </div>
