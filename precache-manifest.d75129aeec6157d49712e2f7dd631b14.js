c5a98cdcde9aeabde963635ca25ad4af326d2868
camerongraff <camerongraff@mines.edu>
2020-03-23 18:09:16 -0600

    Updates

---
+++ precache-manifest.d75129aeec6157d49712e2f7dd631b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bd53eb0cb18c3d0f293fece1d9f6b8",
+    "url": "/checkers/index.html"
+  },
+  {
+    "revision": "32ec910291c60acd3de3",
+    "url": "/checkers/static/css/main.197cd925.chunk.css"
+  },
+  {
+    "revision": "53ce432bae84a55af2b7",
+    "url": "/checkers/static/js/2.83756311.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/checkers/static/js/2.83756311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ec910291c60acd3de3",
+    "url": "/checkers/static/js/main.094940e9.chunk.js"
+  },
+  {
+    "revision": "89aef6b1990740e489fa",
+    "url": "/checkers/static/js/runtime-main.d47ce47d.js"
+  }
+]);
