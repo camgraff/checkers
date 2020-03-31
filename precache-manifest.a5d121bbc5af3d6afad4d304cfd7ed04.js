37501ebc7b27bf311a7fcabe24ba42730ef4e5ed
camgraff <46980357+camgraff@users.noreply.github.com>
2020-03-31 04:32:45 +0000

    Deploying to gh-pages from  - 41c2f093cfcf2d44373c75755ad7f8b4780d9381 🚀

---
+++ precache-manifest.a5d121bbc5af3d6afad4d304cfd7ed04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc57fdc0e4fc2effbbaefa0b6a289ab0",
+    "url": "/checkers/index.html"
+  },
+  {
+    "revision": "4247489081ed5b61e2ad",
+    "url": "/checkers/static/css/main.ab002cc6.chunk.css"
+  },
+  {
+    "revision": "f86427f0c1b4c0adf7ec",
+    "url": "/checkers/static/js/2.934b296a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/checkers/static/js/2.934b296a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4247489081ed5b61e2ad",
+    "url": "/checkers/static/js/main.9f45cdb8.chunk.js"
+  },
+  {
+    "revision": "89aef6b1990740e489fa",
+    "url": "/checkers/static/js/runtime-main.d47ce47d.js"
+  }
+]);
